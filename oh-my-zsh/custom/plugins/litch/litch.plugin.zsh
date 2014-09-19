d9babfe563007ea59b05d93f1db8365746b712fb
Justin Litchfield <litch@me.com>
2014-09-19 14:27:57 -0500

    steal that c to change directories to code function

--- oh-my-zsh/custom/plugins/litch/litch.plugin.zsh
+++ oh-my-zsh/custom/plugins/litch/litch.plugin.zsh
@@ -3,3 +3,14 @@ alias gst='git status'
 unalias gm
 
 alias gcp='git commit -p'
+
+c() { cd ~/code/$1; }
+_c() { _files -W ~/code -/; }
+compdef _c c
+
+h() { cd ~/$1; }
+_h() { _files -W ~/ -/; }
+compdef _h h
+
+# autocorrect is more annoying than helpful
+unsetopt correct_all
